b410f776d3c9e7cf43880900696e5c15754d434c
ErenD18 <donemecli18@gmail.com>
2025-10-30 11:21:01 +0300

    Add files via upload

---
+++ GirisYap.php
@@ -0,0 +1,8 @@
+<form>
+    <div style="display: flex; justify-content: center; flex-direction: column; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+        <h1 style="margin-bottom: 20px; color: white; align-self: flex-start;">GİRİŞ YAP</h1>
+        <input type="text" placeholder="Kullanıcı Adı">
+        <input type="text" placeholder="Şifre">
+        <button type="button" style="margin-bottom: 20px; align-self: center; text-align: center;">GİRİŞ YAP</button> 
+    </div>
+</form>
